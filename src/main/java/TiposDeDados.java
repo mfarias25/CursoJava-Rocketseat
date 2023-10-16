39bcee0e3b7d4f24a194c1da5581885cdbb6b6c2
Mateus Farias <mateusfariasdev@outlook.com>
2023-10-16 16:13:32 -0300

    tipos de dados

---
+++ src/main/java/TiposDeDados.java
@@ -0,0 +1,16 @@
+public class TiposDeDados {
+    public static void main(String[] args) {
+
+        //Numeros(int, double, float, long)
+        int dadoDoTipoInt = 10; //valor inteiro com quebra.
+        double dadoDoTipoDouble = 3.50; //valores flutuantes
+        float dadoDoTipoFloat = 1.0f; // valores flutuantes
+        long dadoDoTipoLong = 100000L; // inteiro com valores maiores (longos)
+
+        //Texto(String)
+        String textoTipo = "Hello Dev"; //Text entre aspas duplas, se for apas simples, será um Char
+
+        //Boleano(boolean = true or false)
+        boolean dadoTipoBoleano = true; // true or false sem aspas
+    }
+}
