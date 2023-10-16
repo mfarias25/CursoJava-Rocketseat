98355cd7e120bd51bbd67dcdf367d761eae0db24
Mateus Farias <mateusfariasdev@outlook.com>
2023-10-16 16:01:41 -0300

    estrutura de classe, Main

---
+++ src/main/java/PrimeiraClasse.java
@@ -0,0 +1,26 @@
+public class PrimeiraClasse {
+    /*Classe: Definimos Objetos, Metodos, Atributos...
+- Nome da classe, igual nome do arquivo
+- public, private, protected = Tipo de acesso da classe
+- class = tipo de classe
+ */
+    //-------------------
+    public static void main(String[] args) {
+/*
+- public, private, protected = Tipo de acesso da classe
+-void = executa uma ação mas não retorna nenhuma valor
+-main = Ponto de partido, ponto inicial do programa
+-strings = tipo de conteudo que estamos passando, com [] array de strings
+-args = argumentos que ira receber dentro do metodo main
+ */
+}
+
+}
+//Fora do escopo da classe
+
+
+
+
+
+
+
