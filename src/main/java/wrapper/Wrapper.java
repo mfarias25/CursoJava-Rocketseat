7485bbb76e843e67ea6404c33568b212af05e44c
Mateus Farias <mateusfariasdev@outlook.com>
2023-10-23 16:23:48 -0300

    wrapper classes

---
+++ src/main/java/wrapper/Wrapper.java
@@ -0,0 +1,18 @@
+package wrapper;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        int tipoInt = 10;
+        float tipoFloat = 1;
+        boolean tipoBool = false;
+
+        //int, char
+
+        Float tipoFloat2 = 1F;
+        Integer tipoInt2 = 10;
+        Boolean tipoBool2 = false;
+
+        tipoInt2.parseInt("1");
+
+    }
+}
